386c6ac760bfa1849366e0afbb19bf37c8e6a5a7
brs14shn <brsshn292929@gmail.com>
2022-06-13 15:55:35 +0300

    finished

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-const button=document.querySelector("#btn");
+const button = document.querySelector("#btn");
+const innerContainer = document.querySelector(".inner-container");
+// https://randomuser.me/api
 
-
-const  innerContainer=document.querySelector(".inner-container");
-
-button.addEventListener("click",()=>{
-const getPersondata=async()=>{
-
-    try{
-        const url=`https://randomuser.me/api`
-        let res=await fetch(url)
-        let data=await res.json()
-        console.log(data);
-        createPerson(data);
-
-    }catch(error){
-        alert(error)
-
+button.addEventListener("click", () => {
+  const getPersonData = async () => {
+    try {
+      const url = `https://randomuser.me/api`;
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+      createPerson(data);
+    } catch (error) {
+      alert(error);
     }
-}
-getPersondata();
-})
+  };
+  const createPerson = (data) => {
+    let { first, last } = data.results[0].name;
+    // let name = data.results[0];
+    // let { country } = data.results[0].location;
+    let location = data.results[0].location.country;
+    let { email, phone } = data.results[0];
+    let image=data.results[0].picture.medium;
+
+    innerContainer.innerHTML=` 
+    <img class="image" src="${image}" alt="" />
+    <p>
+        <i class="fa-solid fa-user"></i> Name :
+        <span class="name"> ${first}</span>
+    </p>
+    <p>
+        <i class="fa-solid fa-user"></i> LastName :
+        <span class="last-name"> ${last} </span>
+    </p>
+    <p>
+        <i class="fa-solid fa-magnifying-glass-location"></i> Location :
+        <span class="location">${location}</span>
+    </p>
+    <p>
+        <i class="fa-solid fa-phone-flip"></i>Phone :
+        <span class="phone">${phone}</span>
+    </p>
+    <p>
+        <i class="fa-solid fa-envelope"></i>Email :
+        <span class="email"> ${email}</span>
+    </p>`
+
+  console.log(first);
+
+  };
+  getPersonData();
+});
